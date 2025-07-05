56c795f5fd599e9f0d43216fe1393b31f8dbb84e
Mahendra Mane <mahendramane070707@gmail.com>
2025-07-06 02:28:10 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -13,8 +13,8 @@ terraform {
 provider "azurerm" {
   features {}
 
-  client_id       = "5771711c-32a4-4d4e-b54b-c4f4bf4ee9f0"
-  client_secret   = "1hq8Q~FHe4ZxIAJF2OUnI3v3O6XpccZMIXIpNbu7"
-  tenant_id       = "1d5f22a5-9a8c-40c6-b572-d73153c70502"
-  subscription_id = "af33ec65-f616-47bd-a12b-6e7d22a62bb2"
+  client_id       = "ee9f0"
+  client_secret   = "XIpNbu7"
+  tenant_id       = "1d5f2"
+  subscription_id = "2bb2"
 }
